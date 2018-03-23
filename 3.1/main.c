07190f47616c77d7a7209a22c26c47dbf5683d6a
divanovTest <divanov@mera.ru>
2018-03-23 16:37:25 +0400

    v3

--- 3.1/main.c
+++ 3.1/main.c
@@ -15,17 +15,17 @@ int pow2(int p)
 // Возвращает факториал числа n:
 int fact(int n)
 {
-	return n<=1 ? 1 : n * fact(n-1);
+	return n <= 1 ? 1 : n * fact( n-1 );
 }
 // Возвращает сумму цифр числа. sum_digit(123) = 6:
 int sum_digit(int n)
 {
-	return n>0?n%10+sum_digit(n/10):0;
+	return n > 0 ? n % 10 + sum_digit( n/10 ) : 0;
 }
 // Возвращает число ненулевых битов в n. nz_bits(15121) = 7 :
 int nz_bits(int n)
 {
-	return n>0?n%2!=0?1+nz_bits(n/2):0+nz_bits(n/2):0;
+	return n > 0 ? n % 2 != 0 ? 1 + nz_bits( n/2 ) : 0 + nz_bits( n/2 ) : 0;
 }
 
 void main(void){
